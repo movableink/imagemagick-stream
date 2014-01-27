01c1f2d002c3bcfe9f90617dac07988a7504f3e1
Michael Nutt <michael@nuttnet.net>
2014-01-27 11:28:55 -0500

    copy of now-missing imagemagick-stream repo

---
+++ index.js
@@ -0,0 +1,224 @@
+var spawn = require('child_process').spawn
+  , stream = require('stream')
+  , Duplex = stream.Duplex
+  , Readable = stream.Readable
+  , isError = require('util').isError
+  , fs = require('fs')
+  , util = require('util')
+
+// Node pre v0.10.0 comp.
+if (!Duplex) Duplex = require('readable-stream').Duplex;
+if (!Readable) Readable = require('readable-stream').Readable;
+
+function ImageMagick (src) {
+  if (!(this instanceof ImageMagick))
+    return new ImageMagick(src);
+  
+  Duplex.call(this);
+  this.source = undefined;
+  this.args = ['-'];
+  this.imgout = "";
+  src && this.from(src);
+  process.nextTick(_spawn.bind(this));
+}
+  
+ImageMagick.prototype = {
+  __proto__: Duplex.prototype,
+
+  Stream : function(src) {
+      return new ImageMagick(src)
+  },
+
+  /**
+   * Implementing _read
+   *
+   * @api private
+   */
+  
+  _read: function (n) {
+    if (!this.source) return this.push('');
+    var chunk = this.source.read(n);
+    this.push(chunk || '');
+  },
+  
+  /**
+   * Implementing _write
+   *
+   * @api private
+   */
+  
+  _write: function (chunk, encoding, callback) {
+    if (this.dest) {
+      this.dest.write(chunk, encoding, callback);
+      return;
+    }
+    
+    this.once('spawn', function () {
+      this._write(chunk, encoding, callback);
+    });
+  },
+  
+  /**
+   * Sets the `quality` option
+   *
+   * @param {String|Number} args
+   * @api public
+   */
+  
+  quality: function (args) {
+    this.args.push('-quality', args);
+    return this;
+  },
+  
+  /**
+   * Sets the `resize` option
+   *
+   * @param {String} args
+   * @api public
+   */
+   
+  resize: function (args) {
+    this.args.push('-resize', args);
+    return this;
+  },
+  
+  /**
+   * Sets the `scale` option
+   *
+   * @param {String} args
+   * @api public
+   */
+   
+  scale: function (args) {
+    this.args.push('-scale', args);
+    return this;
+  },
+  
+  /**
+   * Sets the `crop` option
+   *
+   * @param {String} args
+   * @api public
+   */
+  
+  crop: function (args) {    
+    this.args.push('-crop', args);
+    return this;
+  },
+  
+  /**
+   * Sets the `gravity` option
+   *
+   * @param {String} args
+   * @api public
+   */
+  
+  gravity: function (args) {
+    this.args.push('-gravity', args);
+    return this;
+  },
+  
+  /**
+   * Sets the `thumbnail` option
+   *
+   * @param {String} args
+   * @api public
+   */
+  
+  thumbnail: function (args) {
+    this.args.push('-thumbnail', args);
+    return this;
+  },
+ 
+
+  /**
+   *sets the 'imgtype' option
+   *
+   *@param {string} type
+   *@api public
+   */
+
+  imgtype: function(type) {
+    this.imgout = type + ':';  
+    return this;
+  },
+
+  /** 
+   *sets the autorotation option
+   *
+   *@param {boolean} type
+   *@api public
+   */ 
+
+   autorotation: function(rotate) {
+     if(rotate){
+       this.args.push('-auto-orient');
+     }
+     return this;
+   },
+  /**
+   *Read image data from path
+   *
+   *@param {string} path
+   *@api public
+   */
+
+
+  from: function (path) {
+    var read = fs.createReadStream(path);
+    read.on('error', _onerror.bind(this))
+    read.pipe(this);
+    return this;
+  },
+  
+  /**
+   * Write image data to path
+   *
+   * @param {String} path
+   * @api public
+   */
+   
+  to: function (path) {
+    var write = fs.createWriteStream(path);
+    write.on('error', _onerror.bind(this))
+    this.pipe(write);
+    return this;
+  }
+};
+
+function _spawn () {
+  var onerror = _onerror.bind(this);
+  this.args.push(this.imgout + '-');
+  
+  var proc = spawn('convert', this.args);
+  
+  var stdout = proc.stdout;
+  
+  if (!stdout.read) {
+    stdout = new Readable();
+    stdout.wrap(proc.stdout);
+  }
+  
+  stdout.on('end', this.push.bind(this, null));
+  stdout.on('readable', this.read.bind(this, 0));
+  stdout.on('error', onerror);
+    
+  var stderr = proc.stderr;
+  stderr.on('data', onerror);
+  stderr.on('error', onerror);
+  
+  var stdin = proc.stdin;
+  stdin.on('error', onerror);
+  this.on('finish', stdin.end.bind(stdin));
+  
+  this.source = stdout;
+  this.dest = stdin;
+  this.emit('spawn', proc);
+}
+
+function _onerror (err) {
+  if (!isError(err)) err = new Error(err.toString());
+  this.emit('error', err);
+}
+
+module.exports = ImageMagick;
---
+++ readme.md
@@ -0,0 +1,37 @@
+# imagemagick-stream
+
+Streaming imagemagick api
+
+## Use
+    $ npm install imagemagick-stream
+
+```js
+var im = require('imagemagick-stream');
+var in = fs.createReadStream('image.png');
+var out = fs.createWriteStream('image-resized.png');
+  
+var resize = im().resize('200x200').quality(90);
+
+in.pipe(resize).pipe(out);
+  
+// Alternatively
+im('image.png')
+  .resize('200x200')
+  .quality(90)
+  .pipe(out);
+  
+// Or
+im('image.png')
+  .resize('200x200')
+  .quality(90)
+  .to('image-resized.png');
+```
+
+## Todo
+- More options
+Added 
+.imagetype(type)
+.autorotate(true)
+## License 
+
+MIT
